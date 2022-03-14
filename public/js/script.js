a41c62ff9f2f11a86b8c355ec49bf3639ed0cc87
Eller Jones Silveira <ellernetpar@gmail.com>
2022-03-14 16:47:12 -0300

    .

--- public/js/script.js
+++ public/js/script.js
@@ -16,7 +16,28 @@ $(document).ready(function() {
 
     $('.real').maskMoney({precision: 2, decimal:',', thousands: ''});
 
+    var MercoSulMaskBehavior = function (val) {
+        var myMask = 'SSS0A00';
+        var mercosul = /([A-Za-z]{3}[0-9]{1}[A-Za-z]{1})/;
+        var normal = /([A-Za-z]{3}[0-9]{2})/;
+        var replaced = val.replace(/[^\w]/g, '');
+        if (normal.exec(replaced)) {
+            myMask = 'SSS-0000';
+        } else if (mercosul.exec(replaced)) {
+            myMask = 'SSS0A00';
+        }
+        return myMask;
+    },
+
+    mercoSulOptions = {
+        onKeyPress: function(val, e, field, options) {
+            field.mask(MercoSulMaskBehavior.apply({}, arguments), options);
+        }
+    };
+
     $('[name="postal_code"]').mask('00000-000');
+    $('[name="year_fab_mod"]').mask('0000/0000');
+    $('[name="plate_car"]').addClass('text-uppercase').mask(MercoSulMaskBehavior, mercoSulOptions);
 
     $('[name="cpf"]').mask('000.000.000-00');
     $('[name="cnpj"]').mask('00.000.000/0000-00');
