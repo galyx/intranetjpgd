6821750467467372f7821462ba91bb8e81381309
Eller Jones Silveira <ellernetpar@gmail.com>
2022-06-06 12:00:18 -0300

    .

--- app/Http/Controllers/GeralController.php
+++ app/Http/Controllers/GeralController.php
@@ -638,7 +638,7 @@ class GeralController extends Controller
     // ------------------------------
     public function saveClient($request)
     {
-        \Log::info(['request', 'saveCliente', $request->all()]);
+        // \Log::info(['request', 'saveCliente', $request->all()]);
         if(isset($request->client_foto)) $request->foto = $request->client_foto;
         $client_create['lojista_id'] = isset($request->particular) ? 0 : $request->lojista_id;
         $client_create['particular'] = isset($request->particular) ? 1 : 0;
@@ -661,19 +661,19 @@ class GeralController extends Controller
         if(isset($request->client_id)){
             Client::find($request->client_id)->update($client_create);
             $client = Client::find($request->client_id);
-            \Log::info(['if:01', 'saveCliente', $client]);
+            // \Log::info(['if:01', 'saveCliente', $client]);
         }else{
             if(Client::where('lojista_id', isset($request->particular) ? 0 : $request->lojista_id)->where('document_number', $request->document_number)->get()->count() > 0){
                 Client::where('lojista_id', isset($request->particular) ? 0 : $request->lojista_id)->where('document_number', $request->document_number)->update($client_create);
-                $client = Client::where('lojista_id', $request->lojista_id)->where('document_number', $request->document_number)->first();
-                \Log::info(['if:02', 'saveCliente', $client]);
+                $client = Client::where('lojista_id', isset($request->particular) ? 0 : $request->lojista_id)->where('document_number', $request->document_number)->first();
+                // \Log::info(['if:02', 'saveCliente', $client]);
             }else{
                 $client = Client::create($client_create);
-                \Log::info(['else:02', 'saveCliente', $client]);
+                // \Log::info(['else:02', 'saveCliente', $client]);
             }
         }
 
-        \Log::info(['Client:class', 'saveCliente', $client]);
+        // \Log::info(['Client:class', 'saveCliente', $client]);
 
         $client_id = $request->client_id ?? $client->id;
 
@@ -730,11 +730,13 @@ class GeralController extends Controller
         }else{
             if(Veiculo::where('lojista_id', isset($request->particular) ? 0 : $request->lojista_id)->where('renavam', $request->renavam)->get()->count() > 0){
                 Veiculo::where('lojista_id', isset($request->particular) ? 0 : $request->lojista_id)->where('renavam', $request->renavam)->update($veiculo_create);
-                $veiculo = Veiculo::where('lojista_id', $request->lojista_id)->where('renavam', $request->renavam)->first();
+                $veiculo = Veiculo::where('lojista_id', isset($request->particular) ? 0 : $request->lojista_id)->where('renavam', $request->renavam)->first();
+
             }else{
                 $veiculo = Veiculo::create($veiculo_create);
             }
         }
+
 
         $veiculo_id = $request->veiculo_id ?? $veiculo->id;
 
